06569fd85d86694c7793fa880bf52c2e33ea8aa3
Mast3rChief <gu.schroeer@gmail.com>
2015-04-04 12:00:39 +0200

    added functions

--- qlib.py
+++ qlib.py
@@ -3,131 +3,157 @@ import socket
 import sys
 import json
 
+try:
+	IP = socket.gethostbyname('bellnet')
+	PORT = 11600
+except:
+	print 'Error', 'Could not find QStation!'
 
 class UdpClient:
-    def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
-        self.sock = None
+	def __init__(self, ip, port):
+		self.ip = ip
+		self.port = port
+		self.sock = None
 
-    def send_request(self, cmd):
-        try:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.sock.connect((self.ip, self.port))
+	def send_cmd(self, cmd):
+		try:
+			self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			self.sock.connect((self.ip, self.port))
 
-            if sys.version_info < (3, 0):
-                self.sock.sendall(cmd)
-                data = self.sock.recvfrom(2048)
-                self.sock.close()
+			if sys.version_info < (3, 0):
+				self.sock.sendall(cmd)
+				data = self.sock.recvfrom(2048)
+				self.sock.close()
 
-                # print(cmd + ' : ' + data[0])
+				# print(cmd + ' : ' + data[0])
 
-                return data[0]
-            else:
-                self.sock.sendall(cmd.encode('UTF-8'))
-                data = self.sock.recv(2048)
-                self.sock.close()
+				return data[0]
+			else:
+				self.sock.sendall(cmd.encode('UTF-8'))
+				data = self.sock.recv(2048)
+				self.sock.close()
 
-                # print(cmd + ' : ' + str(data.decode('UTF-8')))
+				# print(cmd + ' : ' + str(data.decode('UTF-8')))
 
-                return str(data.decode('UTF-8'))
-        except:
-            print('UDP Connection Error: ', sys.exc_info()[0])
-            raise
+			out = str(data.decode('UTF-8'))
+		except:
+			print('UDP Connection Error: ', sys.exc_info()[0])
+			raise
 
-    def set_light(self, bright, red, green, blue, status, active_bulb):
-        cmd = '{"cmd":"light_ctrl",' \
-              '"r":"' + str(red) + '",' \
-              '"g":"' + str(green) + '",' \
-              '"b":"' + str(blue) + '",' \
-              '"bright":"' + str(bright) + '",' \
-              '"effect":"9",' \
-              '"iswitch":"' + str(status) + '",' \
-              '"matchValue":"0",' \
-              '"sn_list":[' \
-              '{"sn":"' + active_bulb + '"}' \
-              ']}'
+		try:
+			json_data = json.loads(out)
+			return json_data
+		except:
+			print('JSON Parsing Error: ', sys.exc_info()[0])
+			raise
 
-        data = self.send_request(cmd)
+	def set_light(self, *arguments):
+		cmd =	'{"cmd":"light_ctrl",' \
+				'"r":"' + str(red) + '",' \
+				'"g":"' + str(green) + '",' \
+				'"b":"' + str(blue) + '",' \
+				'"bright":"' + str(bright) + '",' \
+				'"effect":"9",' \
+				'"iswitch":"' + str(status) + '",' \
+				'"matchValue":"0",' \
+				'"sn_list":[' \
+				'{"sn":"' + active_bulb + '"}' \
+				']}'
+		return self.send_cmd(cmd)
 
-    def set_title(self, sn, title):
-        cmd = '{"cmd":"set_title","sn":"' + sn + '","title":"' + title + '"}'
+	def set_title(self, sn, title):
+		cmd = '{"cmd":"set_title","sn":"' + sn + '","title":"' + title + '"}'
+		return self.send_cmd(cmd)
 
-        data = self.send_request(cmd)
+	def get_lights(self):
+		cmd = '{"cmd":"light_list"}'
+		return self.send_cmd(cmd)
 
-    def get_lights(self):
-        cmd = '{"cmd":"light_list"}'
+	def get_groups(self):
+		cmd = '{"cmd":"group_list"}'
+		return self.send_cmd(cmd)
 
-        data = self.send_request(cmd)
+	def get_group_lights(self, group_id):
+		cmd =	'{"cmd":"group_leds_list",' \
+				'"group_id":"' + group_id + '"}'
+		return self.send_cmd(cmd)
 
-        try:
-            json_data = json.loads(data)
+	def set_group(self, *arguments):
+		cmd =	'{"cmd":"<cmdValue>",' \
+				'"r":"<redValue>",' \
+				'"g":"<greenValue>",' \
+				'"b":"blueValue",' \
+				'"bright": "<lightValue>",' \
+				'"effect": "<effectValue>", ' \
+				'"angle": "colorAngle", ' \
+				'"sn_list": [ { "sn": "<xxx>" } ], ' \
+				'"matchValue": "<value>", ' \
+				'"iswitch": "<ledStatus>", ' \
+				'"group1": "<group1Value>", ' \
+				'"angle": "colorValue"}'
+		return self.send_cmd(cmd)
 
-            return json_data
-        except:
-            print('JSON Parsing Error: ', sys.exc_info()[0])
-            raise
+	def create_group(self, group_title):
+		cmd =	'{"cmd":" cmdValue",' \
+				'"group_title":"' + group_title + '"}'
+		return self.send_cmd(cmd)
 
-    def get_groups(self):
-        cmd = '{"cmd":"group_list"}'
+	def delete_group(self, group_id):
+		cmd =	'{"cmd":"del_group",' \
+				'"group_id":"' + group_id + '"}'
+		return self.send_cmd(cmd)
 
-        data = self.send_request(cmd)
+	def add_light2group(self, sn, group_id):
+		cmd =	'{"cmd":"set_group",' \
+				'"sn":"' + sn + '",' \
+				'"group_id":"' + group_id + '"}'
+		return self.send_cmd(cmd)
 
-        try:
-            json_data = json.loads(data)
+	def rmv_light(self, sn, group_id):
+		cmd =	'{"cmd":" leave_group ",' \
+				'"sn":"' + sn + '",' \
+				'"group_id":"' + group_id + '"}'
+		return self.send_cmd(cmd)
 
-            return json_data
-        except:
-            print('JSON Parsing Error: ', sys.exc_info()[0])
-            raise
+	def rename_group(self, sn, group_title):
+		cmd =	'{"cmd":" set_group_title ",' \
+				'"sn":"' + sn + '",' \
+				'"group_title":"' + group_title + '"}'
+		return self.send_cmd(cmd)
 
-    def get_group_lights(self, group_id):
-        cmd = '{"cmd":"group_leds_list",' \
-              '"group_id":"' + group_id + '"}'
 
-        data = self.send_request(cmd)
-
-        try:
-            json_data = json.loads(data)
-
-            return json_data
-        except:
-            print('JSON Parsing Error: ', sys.exc_info()[0])
-            raise
 
 class QStation:
-	PORT = 11600
 	def __init__(self):
 		self.ip = '0'
 		self.bulb = []
 		self.group = []
 		self.group_elements = []
-		try:
-			self.ip = socket.gethostbyname('bellnet')
-		except:
-			print 'Error', 'Could not find QStation!'
+		self.udp_client = UdpClient(IP, PORT)
+		self.get_bulbs()
+		self.get_groups()
 
-		self.get_bulbs(self.ip)
-		self.get_groups(self.ip)
-
-	def get_bulbs(self, ip):
-		self.udp_client = UdpClient(self.ip, self.PORT)
+	def get_bulbs(self):
+		print self.udp_client
 		self.output = self.udp_client.get_lights()
-		
-		bulb_num = len(self.output['led'])
-		self.bulbs = [Bulb(self.output['led'][i]) for i in range(bulb_num)]
+		print self.output
+		print self.output['led']
+		self.bulbs = [ Bulb( self.output['led'][i] ) for i in range( len(self.output['led']) ) ]
 
-	def get_groups(self, ip):
-		self.udp_client = UdpClient(self.ip, self.PORT)
+	def get_groups(self):
 		self.groups = self.udp_client.get_groups()
+		self.group = [Group(self.groups['data'][i], self.udp_client.get_group_lights(self.groups['data'][i]['group_id'])['data']) for i in range(self.groups['data'])]
 
-		group_num = len(self.groups['data'])
-		self.group = [Group(self.groups['data'][i], self.udp_client.get_group_lights(self.groups['data'][i]['group_id'])['data']) for i in range(group_num)]
+	def create_group(self, name):
+		self.udp_client.create_group(name)
+		self.get_groups()
+
+	def delete_group(self, id):
+		self.group[id].delete()
 
 	def show(self):
 		for item in self.bulbs:
 			item.show()
-
 		for item in self.group:
 			item.show()
 
@@ -138,28 +164,39 @@ class Group:
 		for bulb in elements:
 			if 'status' in bulb and bulb['status'] == '1':
 				self.elements.append(bulb['sn'])
+		self.udp_client = UdpClient(IP, PORT)
 
 	def get_list(self):
+		return self.elements
 
-	def add2list(self, sn):
+	def add_bulb2list(self, sn):
+		self.udp_client.add_light2group(sn, self.settings['group_id'])
 
-	def rmv(self, sn):
+	def rmv_blub(self, sn):
+		self.udp_client.rmv_light(sn, self.settings['group_id'])
 
 	def switch(self, bright, angle):
+		self.udp_client.set_group([bright, angle])
 
 	def switch_status(self):
+		return True
 
 	def show(self):
-		print self.settings['group_id'], self. settings['group_title']
+		print self.settings['group_id'], self.settings['group_title']
 		print self.elements
+
+	def delete(self):
+		self.udp_client.del_group(self.settings['group_id'])
 
 class Bulb:
 	def __init__(self, settings):
 		self.settings = settings
 
 	def switch(self, bright, angle):
+		self.udp_client.set_light([bright, angle])
 
 	def switch_status(self):
+		return True
 
 	def show(self):
 		print self.settings['sn'], self.settings['title']
