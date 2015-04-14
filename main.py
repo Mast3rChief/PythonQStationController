a33e8e6224ce545fccaf3e26157a1804fddd59bf
Mast3rChief <gu.schroeer@gmail.com>
2015-04-14 18:26:19 +0200

    Fixed the error and everything works fine again

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 from udp_client import *
 import sys
-import socket
+from ConfigParser import SafeConfigParser
 
 if sys.version_info < (3, 0):
     from Tkinter import *
     from tkMessageBox import *
     from tkColorChooser import askcolor
-    from ConfigParser import SafeConfigParser
     import ttk as ttk
 else:
     from tkinter import *
     from tkinter.messagebox import *
     from tkinter.colorchooser import askcolor
-    from configparser import SafeConfigParser
     import tkinter.ttk as ttk
 
 
 class App:
-    NAME = 'PythonQStationController'
-    TITLE = 'Q Station Controller'
     BACKGROUND = '#efefef'
-    CONFIG_FILE = 'config.ini'
     PORT = 11600
 
     def __init__(self):
-        self.root = Tk(className=self.TITLE)
-        self.root.title(self.TITLE)
+        self.root = Tk(className='Q Station Controller')
+        self.root.title('Q Station Controller')
         self.root.resizable(0, 0)
         self.root.configure(background=self.BACKGROUND)
         icon = PhotoImage(file='icons/icon.gif')
@@ -37,7 +32,6 @@ class App:
         self.mainframe.rowconfigure(0, weight=1)
         self.mainframe.configure(background=self.BACKGROUND)
 
-        self.udp_client = NONE
         self.ip = StringVar()
         self.name = StringVar()
         self.color = ()
@@ -47,15 +41,12 @@ class App:
         self.item_id = IntVar()
 
         self.config = SafeConfigParser()
-        self.config.read(self.CONFIG_FILE)
-
-        try:
-            self.ip.set(socket.gethostbyname('belllnet'))
-        except:
-            if any(self.NAME in sec for sec in self.config.sections()):
-                self.ip.set(self.config.get(self.NAME, 'last_ip'))
-            else:
-                self.config.add_section(self.NAME)
+        self.config.read('config.ini')
+        print self.config.sections()
+        if any('last_Qstation' in sec for sec in self.config.sections()):
+            self.ip.set(self.config.get('last_Qstation', 'ip'))
+        else:
+            self.config.add_section('last_Qstation')
 
         self.bulb_treeview = ttk.Treeview(self.mainframe)
         self.bulb_treeview.heading("#0", text="Bulbs")
@@ -70,23 +61,15 @@ class App:
         img_connect = PhotoImage(file='icons/arrow_refresh.gif')
         Button(self.mainframe, command=self.callback_get_bulbs, image=img_connect).grid(column=3, row=0, sticky=(W, E))
 
-        #Button(self.mainframe, command=self.callback_get_groups, image=img_connect).grid(column=3, row=10, sticky=(W, E))
-
         self.labelframe = LabelFrame(self.mainframe, text='Bulb Settings')
         self.labelframe.grid(column=1, columnspan=3, row=1, sticky=(N, W, E, S))
 
-        Label(self.labelframe, text='Name').grid(column=1, row=1, sticky=W)
-
         self.name_entry = Entry(self.labelframe, textvariable=self.name)
-        self.name_entry.grid(column=2, row=1, sticky=(W, E))
-
-        Label(self.labelframe, text='Brightness').grid(column=1, row=3, sticky=W)
+        self.name_entry.grid(column=2, columnspan=2, row=1, sticky=(W, E))
         
-        self.bright_scale = Scale(self.labelframe, from_=0, to=255, orient=HORIZONTAL)
+        self.bright_scale = Scale(self.labelframe, from_=0, to=100, orient=HORIZONTAL)
         self.bright_scale.set(0)
-        self.bright_scale.grid(column=2, row=3, sticky=(N, W, E, S))
-
-        Label(self.labelframe, text='Status').grid(column=1, row=2, sticky=W)
+        self.bright_scale.grid(column=2, columnspan=2, row=3, sticky=(N, W, E, S))
 
         Checkbutton(self.labelframe, text=' Turn the bulb on or off', variable=self.status).grid(column=2,
                                                                                                  columnspan=2,
@@ -95,13 +78,20 @@ class App:
 
         img_accept = PhotoImage(file='icons/accept.gif')
         Button(self.labelframe, text='Set Values', command=self.callback_set_values, image=img_accept,
-               compound=LEFT).grid(column=1, columnspan=2, row=5, sticky=(W, E))
+               compound=LEFT).grid(column=1, columnspan=3, row=5, sticky=(W, E))
 
-        Label(self.labelframe, text='Color').grid(column=1, row=4, sticky=W)
+        self.color_button = Button(self.labelframe, command=self.callback_set_color)
+        self.color_button.grid(column=2, row=4, sticky=(W, E))
 
         img_color = PhotoImage(file='icons/color_wheel.gif')
-        self.color_button = Button(self.labelframe, command=self.callback_set_color, image=img_color)
-        self.color_button.grid(column=2, row=4, sticky=(W, E))
+        Button(self.labelframe, command=self.callback_set_color, image=img_color, height=22).grid(column=3,
+                                                                                                  row=4,
+                                                                                                  sticky=(W, E))
+
+        Label(self.labelframe, text='Name').grid(column=1, row=1, sticky=W)
+        Label(self.labelframe, text='Status').grid(column=1, row=2, sticky=W)
+        Label(self.labelframe, text='Brightness').grid(column=1, row=3, sticky=W)
+        Label(self.labelframe, text='Color').grid(column=1, row=4, sticky=W)
 
         for child in self.mainframe.winfo_children():
             try:
@@ -118,45 +108,37 @@ class App:
             except:
                 pass
 
-        if self.ip.get():
-            self.callback_get_bulbs()
-
         self.ip_entry.focus()
 
         self.root.mainloop()
 
     def callback_set_color(self):
         if self.bulb_treeview.selection() != '' and self.item != 'bulbs':
-            tempcolor = askcolor(color=(int(self.color[0][0]), int(self.color[0][1]), int(self.color[0][2])))
-            if tempcolor[1] is not None:
-                self.color = tempcolor 
-                self.color_button.config(background=self.color[1])
+            self.color = askcolor(color=(int(self.color[0][0]), int(self.color[0][1]), int(self.color[0][2])))
+            self.color_button.config(background=self.color[1])
         else:
             showinfo('Info', 'Please select the bulb you want to control.')
 
     def callback_set_values(self):
         if self.bulb_treeview.selection() != '' and self.item != 'bulbs':
-            self.udp_client.set_light(self.bright_scale.get(),
-                                      self.color[0][0],
-                                      self.color[0][1],
-                                      self.color[0][2],
-                                      self.status.get(),
-                                      self.response['led'][self.item_id]['sn'])
-            self.udp_client.set_title(self.response['led'][self.item_id]['sn'],
-                                      self.name.get())
-            self.callback_get_bulbs()
+            udp_client = UdpClient(self.ip.get(), self.PORT)
+            udp_client.set_light(self.bright_scale.get(),
+                                 self.color[0][0],
+                                 self.color[0][1],
+                                 self.color[0][2],
+                                 self.status.get(),
+                                 self.response['led'][self.item_id]['sn'])
+            udp_client.set_title(self.response['led'][self.item_id]['sn'],
+                                 self.name.get())
         else:
             showinfo('Info', 'Please select the bulb you want to control.')
 
     def callback_get_bulbs(self):
         if self.ip.get() != '':
-            self.udp_client = UdpClient(self.ip.get(), self.PORT)
-            self.response = self.udp_client.get_lights()
+            udp_client = UdpClient(self.ip.get(), self.PORT)
+            self.response = udp_client.get_lights()
 
-            self.config.set(self.NAME, 'last_ip', self.ip.get())
-
-            with open(self.CONFIG_FILE, 'w') as f:
-                self.config.write(f)
+            self.config.set('last_Qstation', 'ip', self.ip.get())
 
             map(self.bulb_treeview.delete, self.bulb_treeview.get_children())
             self.bulb_treeview.insert('', 'end', 'bulbs', text='Q Station', open=True)
@@ -170,53 +152,18 @@ class App:
                 self.bulb_treeview.insert('bulbs',
                                           'end',
                                           text=self.response['led'][i]['title'] + ' (' + status + ')',
-                                          tag=i)
+                                          tag=self.response['led'][i]['sn'])
 
             for child in self.labelframe.winfo_children():
                 child.configure(state='normal')
         else:
             showerror('Error', 'Please fill in the Q Station IP first.')
 
-    #def callback_get_groups(self):
-        # There seems to be an error in this seciton, line 204
-        # if self.ip.get() != '':
-        #     self.udp_client = UdpClient(self.ip.get(), self.PORT)
-        #     self.groups = self.udp_client.get_groups()
-        #     self.lights = self.udp_client.get_groups()
-
-        #     self.config.set(self.NAME, 'last_ip', self.ip.get())
-
-        #     with open(self.CONFIG_FILE, 'w') as f:
-        #         self.config.write(f)
-
-        #     print self.groups['data'][0]['group_title'], self.groups['data'][0]['group_id']
-            
-        #     # map(self.bulb_treeview.delete, self.bulb_treeview.get_children())
-
-
-        #     for i in range(len(self.groups['data'])):
-        #         self.bulb_treeview.insert('', 'end', 'groups', text=self.groups['data'][i]['group_title'], open=True)
-        #         # has to be adjusted for each element of the group
-        #         # if int(self.response['led'][i]['online']) == 1:
-        #         #     status = 'Online'
-        #         # else:
-        #         #     status = 'Offline'
-        #         for n in range(len(self.groups['data'])):
-        #         # self.bulb_treeview.insert('groups',
-        #         #                           'end',
-        #         #                           text=self.groups['data'][i]['group_title'],
-        #         #                           tag=i)
-
-        #     # for child in self.labelframe.winfo_children():
-        #     #     child.configure(state='normal')
-        # else:
-        #     showerror('Error', 'Please fill in the Q Station IP first.')
-
     def callback_bulb_treeview(self, event):
         self.item = self.bulb_treeview.selection()[0]
 
         if self.item != 'bulbs':
-            self.item_id = int(self.bulb_treeview.item(self.bulb_treeview.focus())['tags'][0])
+            self.item_id = int(self.bulb_treeview.selection()[0][3]) - 1
 
             self.name_entry.delete(0, END)
             self.name_entry.insert(0, self.response['led'][self.item_id]['title'])
@@ -232,6 +179,10 @@ class App:
 
     def rgb_to_hex(self, rgb):
         return '#%02x%02x%02x' % rgb
+
+    def __del__(self):
+        with open('config.ini', 'w') as f:
+            self.config.write(f)
 
 if __name__ == '__main__':
     app = App()
